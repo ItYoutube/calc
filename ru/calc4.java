f1fe11ececf30052aca03f21251ac47217d478f7
ItYoutube <79218125+ItYoutube@users.noreply.github.com>
2021-03-24 20:27:23 +0300

    BETA 1
    Program Calc beta 1. Russian and English language.

---
+++ ru/calc4.java
@@ -0,0 +1,18 @@
+package ru;
+
+import java.util.Scanner;
+public class calc4 {
+        public static void main(String[] args){
+        System.out.println("Добро пожаловать в программу Calc!");
+       System.out.println("введите 1 и 2 число");
+       System.out.println("Действие - деление");
+       Scanner scan1 = new Scanner(System.in);
+       int a = scan1.nextInt();
+       Scanner scan2 = new Scanner(System.in);
+       int b = scan2.nextInt();
+       int c = a / b;
+       System.out.println(a + ":" + b + "=" + c);
+        
+        }
+    
+}
